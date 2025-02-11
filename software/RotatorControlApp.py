bcb092208b825b3722688d4803eac2e9e71b15a6
ngergihun <ngergihun@gmail.com>
2025-02-11 13:03:40 +0100

    Thorlabs power sensor

--- software/RotatorControlApp.py
+++ software/RotatorControlApp.py
@@ -10,8 +10,8 @@ import sys
 import yaml
 from PySide6 import QtWidgets
 from PySide6 import QtCore, QtGui
-from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QMessageBox
+from PySide6.QtCore import QTimer, QObject, QThread, Signal, Slot
 import pyqtgraph as pg
 import serial.tools.list_ports
 import numpy as np
@@ -20,6 +20,12 @@ from time import sleep
 import asyncio
 import os
 from qt_material import apply_stylesheet
+
+from datetime import datetime
+from ctypes import cdll,c_long, c_ulong, c_uint32,byref,create_string_buffer,c_bool,c_char_p,c_int,c_int16,c_double, sizeof, c_voidp
+from TLPMX import TLPMX
+import time
+from TLPMX import TLPM_DEFAULT_CHANNEL
 
 # import neaSDK
 try:
@@ -36,6 +42,87 @@ ui_file_path = os.path.join(current_folder,ui_file_name)
 
 uiclass, baseclass = pg.Qt.loadUiType(ui_file_path)
 
+class BasePowerSensor():
+    def connect_to_sensor(self):
+        raise NotImplementedError()
+    
+    def get_power(self):
+        raise NotImplementedError()
+
+
+class ThorlabsPM102A(BasePowerSensor):
+    def __init__(self):
+        self.name = None
+        self.sensor = self.connect_to_sensor(self.get_device_name())
+        self.set_wavelength(10000)
+
+    def get_device_name(self):
+        tlPM = TLPMX()
+        deviceCount = c_uint32()
+        tlPM.findRsrc(byref(deviceCount))
+        # print("Number of found devices: " + str(deviceCount.value))
+        resourceName = create_string_buffer(1024)
+        for i in range(0, deviceCount.value):
+            tlPM.getRsrcName(c_int(i), resourceName)
+            print("Thorlabs power meter", i, ":", c_char_p(resourceName.raw).value)
+            print("")
+        tlPM.close()
+        self.name = c_char_p(resourceName.raw).value
+
+        return resourceName
+    
+    def connect_to_sensor(self, resourceName):
+        tlPM = TLPMX()
+        tlPM.open(resourceName, c_bool(True), c_bool(True))
+
+        message = create_string_buffer(1024)
+        tlPM.getCalibrationMsg(message,TLPM_DEFAULT_CHANNEL)
+        # Enable auto-range mode.
+        # 0 -> auto-range disabled
+        # 1 -> auto-range enabled
+        tlPM.setPowerAutoRange(c_int16(1),TLPM_DEFAULT_CHANNEL)
+
+        # Set power unit to Watt.
+        tlPM.setPowerUnit(c_int16(0),TLPM_DEFAULT_CHANNEL)
+ 
+        print("Connected to PM102A/#### Thorlabs power sensor")
+        # print(f"Last calibration date: {c_char_p(message.raw).value}\n")
+
+        return tlPM
+    
+    def set_wavelength(self,wavelength):
+        wl = c_double(wavelength)
+        self.sensor.setWavelength(wl, TLPM_DEFAULT_CHANNEL)
+    
+    def get_power(self):
+        power =  c_double()
+        self.sensor.measPower(byref(power),TLPM_DEFAULT_CHANNEL)
+        return power.value*1000
+
+class Worker(QObject):
+    progress = Signal(str)
+    send_zero_offset = Signal(float)
+
+    def __init__(self,sensor,rotator):
+        super().__init__()
+        self.sensor = sensor
+        self.rotator = rotator
+
+    @Slot()
+    def measure_zero_offset(self,averages,delay):
+        power_measurements = []
+        count = 0
+        while count < averages:
+            power = self.sensor.get_power()
+            power_measurements.append(power)
+            count+=1
+            time.sleep(delay)
+            self.progress.emit(f"Zero adjust step: {count}")
+            print(f"Zero adjust step: {count}")
+        
+        self.progress.emit(f"Zero adjust done: {np.mean(power_measurements)} mW")
+        self.send_zero_offset.emit(np.mean(power_measurements))
+
 class RotatorApp(uiclass, baseclass):
     def __init__(self):
         super().__init__()
@@ -46,10 +133,17 @@ class RotatorApp(uiclass, baseclass):
         self.connected = False
         self.config = None
 
-        self.SearchCOMports()
+        self.FindRotator()
+        self.powersensor = ThorlabsPM102A()
+        # Create the worker thread
+        self.worker = Worker(sensor=self.powersensor,rotator=self.ro)
+        self.worker_thread = QThread()
+        self.worker.moveToThread(self.worker_thread)
+        self.worker_thread.start()
 
-        self.sensor_value = 0
-        self.setpoint_error = 0
+        self.sensor_value = 0.0
+        self.sensor_offset = 0.0
+        self.setpoint_error = 0.0
         self.dt_sensor = 1000
         
         # setup stylesheet
@@ -79,9 +173,14 @@ class RotatorApp(uiclass, baseclass):
         self.AutoFindPushButton.clicked.connect(self.find_power_minimum)
         self.SetPowerPushButton.clicked.connect(self.findSetPointPower)
         self.connectSNOM.clicked.connect(self.connect_to_neasnom)
+
         if offline_mode:
             self.connectSNOM.setEnabled(False)
             self.statusbar.showMessage(u"\u26A0 nea_tools module not found, you can only use the rotator.")
+
+        self.zero_adjust_button.clicked.connect(lambda: self.worker.measure_zero_offset(10,0.5))
+        self.worker.progress.connect(self.status_bar_update)
+        self.worker.send_zero_offset.connect(self.receive_offset)
 
         # Timer for everything
         self.timer=QTimer(self)
@@ -93,7 +192,7 @@ class RotatorApp(uiclass, baseclass):
         self.lcdNumber_2.display(self.ro.get_angle())
         # self.show()
 
-    def SearchCOMports(self):
+    def FindRotator(self):
             comports = serial.tools.list_ports.comports()
             p = [x for x in comports if 'USB VID:PID=0403:6015 SER=DK0BIUIAA' in x.hwid]
             try:
@@ -112,10 +211,10 @@ class RotatorApp(uiclass, baseclass):
                 button = msg.exec()
 
                 if button == QMessageBox.Ok:
-                    self.SearchCOMports()
+                    self.FindRotator()
                 elif button == QMessageBox.Cancel:
                     self.close()
-                    
+
     def check_config_file(self):   # load config
         with open('config.yaml', 'r') as file:
             self.config = yaml.safe_load(file)
@@ -138,7 +237,7 @@ class RotatorApp(uiclass, baseclass):
         if "nea_tools" not in sys.modules:
             return
 
-        self.path_to_dll = ''# yaml.load('config.yaml')
+        # self.path_to_dll = ''# yaml.load('config.yaml')
         path_to_dll = self.config['path_to_dll']
         fingerprint = self.config['fingerprint']
         host = 'nea-server'
@@ -149,7 +248,7 @@ class RotatorApp(uiclass, baseclass):
             self.SetPowerPushButton.setEnabled(False)
             self.SetPointSpinBox.setEnabled(False)
             self.AutoFindPushButton.setEnabled(False)
-            self.lcdNumber.setEnabled(False)
+            self.lcdNumber.setEnabled(True)
             self.checkBoxSNOM.setChecked(False)
             self.connected = False
             self.statusbar.showMessage("Disconnected from SNOM")
@@ -184,10 +283,14 @@ class RotatorApp(uiclass, baseclass):
         self.LaserTemp = self.m.Temperature
 
     def readSensor(self):
-        if self.connected:
-            self.sensor_value = self.context.Microscope.Py.EnergySensor
-            self.setpoint_error = self.SetPointSpinBox.value() - self.sensor_value
-            self.lcdNumber.display(self.sensor_value)
+        # if self.connected:
+        # self.sensor_value = self.context.Microscope.Py.EnergySensor
+        self.sensor_value = self.powersensor.get_power()-self.sensor_offset
+        self.setpoint_error = self.SetPointSpinBox.value() - self.sensor_value
+        self.lcdNumber.display(self.sensor_value)
+
+    def receive_offset(self,value):
+        self.sensor_offset = value
 
     def jogging(self,direction,speed):
         stepsize = speed*self.JogSizeSpinBox.value()
@@ -265,6 +368,9 @@ class RotatorApp(uiclass, baseclass):
             print("Setpoint reached")
             self.statusbar.showMessage(f"Setpoint reached! P={self.sensor_value}")
 
+    def status_bar_update(self,m):
+        self.statusbar.showMessage(m)
+
     def closeEvent(self, event):
         quit_msg = "Are you sure you want to exit the program?"
         print(quit_msg)
@@ -276,7 +382,14 @@ class RotatorApp(uiclass, baseclass):
                 self.ro.close()
                 print(f'{self.deviceport} port closed')
             except:
-                pass
+                print(f'{self.deviceport} port could NOT be closed')
+
+            try:
+                self.powersensor.close()
+                print(f'{self.powersensor.name} closed')
+            except:
+                print(f'{self.powersensor.name} could NOT be closed')
+
             if self.connected:
                 print('\nDisconnecting from neaServer!')
                 nea_tools.disconnect()
